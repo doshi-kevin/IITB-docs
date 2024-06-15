f9938c7b3bd65eeae25356a6978ccb4906c40df7
neelpatel392003 <155454653+neelpatel392003@users.noreply.github.com>
2024-06-15 10:27:46 +0530

    Add files via upload

---
+++ docs.ts
@@ -0,0 +1,266 @@
+export const docs = [
+  {
+    title: "Getting Started",
+    id: "getting-started",
+    items: [
+      {
+        title: "Introduction",
+        href: "/docs/introduction",
+      },
+      {
+        title: "Installation",
+        href: "/docs/installation",
+      },
+      {
+        title: "Project Structure",
+        href: "/docs/project-structure",
+      },
+    ],
+  },
+  {
+    title: "Applied Actions",
+    id: "building-application",
+    items: [
+      {
+        title: "Data Request Actions",
+        href: "/docs/building-application/dataRequest",
+      },
+      {
+        title: "User Management Actions",
+        href: "#",
+      },
+      {
+        title: "Rendering",
+        href: "#",
+      },
+      {
+        title: "Data fetching",
+        href: "#",
+      },
+      {
+        title: "Optimizing",
+        href: "#",
+      },
+      {
+        title: "Deploying",
+        href: "#",
+      },
+    ],
+  },
+  {
+    title: "Components Schemas",
+    id: "schemas",
+    items: [
+      {
+        title: "AddColumns Schema",
+        href:"/docs/components/AddColumns",
+      },
+      {
+        title: "CreateForm Schema",
+        href:"/docs/components/CreateForm",
+      },
+      {
+        title: "DeleteConfimation Schema",
+        href:"/docs/components/DeleteConfirmation",
+      },
+      {
+        title: "DisplayTable Schema",
+        href:"/docs/components/DisplayTable",
+      },
+      {
+        title: "File Uploader Schema",
+        href:"/docs/components/FileUpload",
+      },
+      {
+        title: "FilteredHistory Schema",
+        href:"/docs/components/FilteredHistory",
+      },
+      {
+        title: "FormSkeleton Schema",
+        href:"/docs/components/FormSkeleton",
+      },
+      {
+        title: "LoginForm Schema",
+        href:"/docs/components/LoginForm",
+      },
+      {
+        title: "LoginProvider Schema",
+        href:"/docs/components/LoginProvider",
+      },
+      {
+        title: "Navbar Schema",
+        href:"/docs/components/Navbar",
+      },
+      {
+        title: "Nothing To Display Schema",
+        href:"/docs/components/NothingToDisplay",
+      },
+      {
+        title: "Push mock data Schema",
+        href:"/docs/components/PushMockData",
+      },
+      {
+        title: "Registartionform Schema",
+        href:"/docs/components/RegistrationForm",
+      },
+      {
+        title: "RequestChange Schema",
+        href:"/docs/components/RequestChange",
+      },
+      {
+        title: "Search Schema",
+        href:"/docs/components/Search",
+      },
+      {
+        title: "Sidebar Schema",
+        href:"/docs/components/Sidebar",
+      },
+      {
+        title: "TableHeading Schema",
+        href:"/docs/components/TableHeading",
+      },
+      {
+        title: "TableSkeleton Schema",
+        href:"/docs/components/TableSkeleton",
+      },
+    ]
+  },
+  {
+    title: "Model Schemas",
+    id: "schemas",
+    items: [
+      {
+        title: "Bus Schema",
+        href: "/docs/models/bus",
+      },
+      {
+        title: "Changes Schema",
+        href: "/docs/models/changes",
+      },
+      {
+        title: "Defaultparam Schema",
+        href: "/docs/models/defaultParams",
+      },
+      {
+        title: "Excitationsystems Schema",
+        href: "/docs/models/excitationSystems",
+      },
+      {
+        title: "Generator Schema",
+        href: "/docs/models/generator",
+      },
+      {
+        title: "Ibr Schema",
+        href: "/docs/models/ibr",
+      },
+      {
+        title: "IccHvdcLink Schema",
+        href: "/docs/models/lccHvdcLink",
+      },
+      {
+        title: "load Schema",
+        href: "/docs/models/load",
+      },
+      {
+        title: "modificationhistory Schema",
+        href: "/docs/models/modificationHistory",
+      },
+      {
+        title: "seriesCapacitor Schema",
+        href: "/docs/models/seriesCapacitor",
+      },
+      {
+        title: "seriesFact Schema",
+        href: "/docs/models/seriesFact",
+      },
+      {
+        title: "shuntCapacitor Schema",
+        href: "/docs/models/shuntCapacitor",
+      },
+      {
+        title: "shuntFact Schema",
+        href: "/docs/models/shuntFact",
+      },
+      {
+        title: "shuntReactor Schema",
+        href: "/docs/models/shuntReactor",
+      },
+      {
+        title: "sungleLineDiagram Schema",
+        href: "/docs/models/singleLineDiagram",
+      },
+      {
+        title: "transformersThreeWinding Schema",
+        href: "/docs/models/transformersThreeWinding",
+      },
+      {
+        title: "transformersTwoWinding Schema",
+        href: "/docs/models/transformersTwoWinding",
+      },
+      {
+        title: "transmissionLine Schema",
+        href: "/docs/models/transmissionLine",
+      },
+      {
+        title: "turbineGovernor Schema",
+        href: "/docs/models/turbineGovernor",
+      },
+      {
+        title: "user Schema",
+        href: "/docs/models/user",
+      },
+      {
+        title: "vscHvdcLink Schema",
+        href: "/docs/models/vscHvdcLink",
+      },
+      {
+        title: "Functions",
+        href: "#",
+      },
+      {
+        title: "Edge Runtime",
+        href: "#",
+      },
+      {
+        title: "create-next-app",
+        href: "#",
+      },
+    ],
+  },
+  {
+    title: "Architecture",
+    id: "arch",
+    items: [
+      {
+        title: "Accesibility",
+        href: "#",
+      },
+      {
+        title: "Fast refresh",
+        href: "#",
+      },
+      {
+        title: "Compiler",
+        href: "#",
+      },
+      {
+        title: "Turbo Runtime",
+        href: "#",
+      },
+      {
+        title: "Supported Browsers ",
+        href: "#",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    id: "community",
+    items: [
+      {
+        title: "Contribution Guide",
+        href: "#",
+      },
+    ],
+  },
+];
